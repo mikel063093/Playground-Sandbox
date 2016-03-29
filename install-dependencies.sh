bfc59c397ed4cea82f8d968193a586b80de887f7
Tudor Luca <luca.mtudor@gmail.com>
2016-03-29 11:23:46 +0300

    script delay

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,7 +6,12 @@ if [ ! -e $DEPS ]; then
     echo y | android update sdk --no-ui --all --filter tool &&
     echo y | android update sdk --no-ui --all --filter tools &&
     echo y | android update sdk --no-ui --all --filter platform-tools &&
-    echo y | android update sdk --no-ui --all --filter build-tools-23.0.3,extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository
+    (while :
+    do
+        echo y
+        sleep 10
+    done) | android update sdk --no-ui --all --filter build-tools-23.0.3 &&
+    echo y | android update sdk --no-ui --all --filter extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository
     touch $DEPS
 fi
 
