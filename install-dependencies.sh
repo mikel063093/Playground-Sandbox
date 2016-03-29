63a52f55dc86b639c00fd077626373eaaa356d97
Tudor Luca <luca.mtudor@gmail.com>
2016-03-29 14:25:38 +0300

    Refactors install-dependencies.sh script. Forces m2repository rev. 26

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-#export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
-#DEPS="$ANDROID_HOME/installed-dependencies"
-
-#if [ ! -e ${DEPS} ]; then
-#    cp -r /usr/local/android-sdk-linux ${ANDROID_HOME} &&
-    echo y | android update sdk --no-ui --all --filter tool &&
-    echo y | android update sdk --no-ui --all --filter tools &&
-    echo y | android update sdk --no-ui --all --filter platform-tools &&
+function updateAndroidSDK() {
+    echo y | android update sdk --no-ui --all --filter tool
+    echo y | android update sdk --no-ui --all --filter tools
+    echo y | android update sdk --no-ui --all --filter platform-tools
     (while :
     do
         echo y
         sleep 10
-    done) | android update sdk --no-ui --all --filter build-tools-23.0.3 &&
+    done) | android update sdk --no-ui --all --filter build-tools-23.0.3
     echo y | android update sdk --no-ui --all --filter extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository
-#    touch ${DEPS}
-#fi
+}
 
-# echo y | android update sdk --no-ui --all --filter "build-tools-23.0.1,extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository"
-# echo y | android update sdk --no-ui --all --filter "build-tools-23.0.3,extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository"
+function installM2RepositoryRev26() {
+    cd ${ANDROID_HOME}/extras/android
+    rm -rf m2repository
+    wget http://dl.google.com/android/repository/android_m2repository_r26.zip
+    unzip android_m2repository_r26.zip
+}
+
+updateAndroidSDK
+installM2RepositoryRev26
