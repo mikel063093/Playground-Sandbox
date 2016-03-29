933346053a2b9c3478687557647eb4a51ab5e30c
Tudor Luca <luca.mtudor@gmail.com>
2016-03-29 14:05:46 +0300

    Removes android sdk from the cache directories list.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+#export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
-DEPS="$ANDROID_HOME/installed-dependencies"
+#DEPS="$ANDROID_HOME/installed-dependencies"
 
-if [ ! -e ${DEPS} ]; then
-    cp -r /usr/local/android-sdk-linux ${ANDROID_HOME} &&
+#if [ ! -e ${DEPS} ]; then
+#    cp -r /usr/local/android-sdk-linux ${ANDROID_HOME} &&
     echo y | android update sdk --no-ui --all --filter tool &&
     echo y | android update sdk --no-ui --all --filter tools &&
     echo y | android update sdk --no-ui --all --filter platform-tools &&
@@ -15,8 +15,8 @@ if [ ! -e ${DEPS} ]; then
         sleep 10
     done) | android update sdk --no-ui --all --filter build-tools-23.0.3 &&
     echo y | android update sdk --no-ui --all --filter extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository
-    touch ${DEPS}
-fi
+#    touch ${DEPS}
+#fi
 
 # echo y | android update sdk --no-ui --all --filter "build-tools-23.0.1,extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository"
 # echo y | android update sdk --no-ui --all --filter "build-tools-23.0.3,extra-android-support,extra-google-google_play_services,extra-google-m2repository,extra-android-m2repository"
